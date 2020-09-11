f4b4597774cac2614d591b4d0f47df2daa48ab95
Dario Carlino <baires82@gmail.com>
2020-09-11 01:05:56 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,16 +1,16 @@
 class Student < User
   attr_accessor :knowledge
-  know = []
+  @@know = []
 
   def initialize
-    @knowledge = know
+    @knowledge = @@know
   end
 
   def learn(string_of_knowledge)
-    know << string_of_knowledge
+    @@know << string_of_knowledge
   end
 
   def knowledge
-    know
+    @@know
   end
 end
